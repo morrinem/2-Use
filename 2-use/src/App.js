13c7486d23bfb24bcff85ef5890264c6f83de1c0
emer289 <murphe65@tcd.ie>
2022-02-12 20:36:54 +0000

    added a login/register route

--- 2-use/src/App.js
+++ 2-use/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LoginRegister from "./pages/LoginRegister"
 /* 
 Replace <Home/> below with <ProductPage/> to view the product page
 I'll link it up with the home page after
@@ -12,6 +13,7 @@ function App() {
     
         <Router>
             <Route path='/' exact component={Home} />
+            <Route path='/loginRegister' exact component={LoginRegister} />
         </Router>
   
 )
---
+++ 2-use/src/pages/LoginRegister.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function LoginRegister () {
+    return <div>Login/Register</div>
+}
+
+export default LoginRegister
