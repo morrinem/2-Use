aab179c43ba9707e836fccd1873f269818157595
emer289 <murphe65@tcd.ie>
2022-03-13 17:28:55 +0100

    when the register button is clicked the profile photo is uploaded

--- 2-use/src/pages/Register.js
+++ 2-use/src/pages/Register.js
@@ -24,14 +24,7 @@ const Register = () => {
 
     const handleImageSubmit = async (e) => {
         e.preventDefault()
-        let formData = new FormData()
-        formData.append('file', image.data)
-        const response = await fetch('http://localhost:3001/auth/image', {
-            method: 'POST',
-            body: formData,
-        })
-
-        if (response) setStatus(response.statusText)
+       
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -73,6 +66,14 @@ const Register = () => {
                 
             });
 
+            let formData = new FormData()
+            formData.append('file', image.data)
+            const response = await fetch('http://localhost:3001/auth/image', {
+                method: 'POST',
+                body: formData,
+            })
+
+            if (response) setStatus(response.statusText)
         } catch (err) {
             console.log("We have some error!");
         }
@@ -147,9 +148,8 @@ const Register = () => {
                 <h1>Profile Photo</h1>
                 {image.preview && <img src={image.preview} width='100' height='100' />}
                 <hr></hr>
-                <form onSubmit={handleImageSubmit}>
+                <form>
                     <input type='file' name='file' onChange={handleFileChange}></input>
-                    <button type='submit'>Submit</button>
                 </form>
                 {status && <h4>{status}</h4>}
 
