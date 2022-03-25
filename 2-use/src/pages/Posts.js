bb00d06629a5be5ae934035a847456a976ec35ad
JamesOC3310 <98289189+JamesOC3310@users.noreply.github.com>
2022-03-25 17:32:49 +0000

    checkout working

--- 2-use/src/pages/Posts.js
+++ 2-use/src/pages/Posts.js
@@ -14,16 +14,42 @@ const Posts = () => {
         )
     },[])
 
+    const handleSubmit = async (e) => {
+        console.log("button pressed")
+        fetch("http://localhost:3001/posts/create-checkout-session", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              items: [
+                { id: 1, quantity: 3 },
+                { id: 2, quantity: 1 },
+              ],
+            }),
+          })
+            .then(res => {
+              if (res.ok) return res.json()
+              return res.json().then(json => Promise.reject(json))
+            })
+            .then(({ url }) => {
+              window.location = url
+            })
+            .catch(e => {
+              console.error(e.error)
+            })
+    }
+
     return (
         <div className="App">
             {listOfPosts.map((value, key) => {
-                return <div className="post" onClick={() => {
-                history.push(`/Post/${value.id}`)
-                }
-                }>
+                return <div className="post" 
+                >
                   <div className="title">{value.title}</div>
                     <div className="body">{value.postText}</div>
-                    <div className="footer">{value.username}</div>
+                    <div className="footer">{value.username}
+                    <button onClick={handleSubmit}>checkout</button>
+                    </div>
                 </div>})}
         </div>
     );
--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express")
 const router = express.Router()
 const { Posts } = require('../models')
@@ -6,6 +7,40 @@ const {verifyJWT} = require('../middlewares/AuthMiddleware')
 router.use(express.json())
 
 
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = new Map([
+  [1, { priceInCents: 10000, name: "Learn React Today" }],
+  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+])
+
+
+router.post("/create-checkout-session", async (req, res) => {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: req.body.items.map(item => {
+          const storeItem = storeItems.get(item.id)
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: storeItem.name,
+              },
+              unit_amount: storeItem.priceInCents,
+            },
+            quantity: item.quantity,
+          }
+        }),
+        success_url: "http://localhost:3000",
+        cancel_url: "http://localhost:3000",
+      })
+      res.json({ url: session.url })
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+  })
 
 router.get("/", async (req, res) => {
     const listOfPosts = await Posts.findAll()
